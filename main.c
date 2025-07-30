9697310c03041fa67fd1c222f75b4a9b0cf108ac
Ahmad Akra <ahakra@vanrise.com>
2025-07-30 17:25:12 +0300

    first chapter

---
+++ main.c
@@ -0,0 +1,17 @@
+// C program to detect Operating System
+
+#include <stdio.h>
+#include <dirent.h>
+#include "apue.h"
+
+// Driver Code
+int main(int argc, char *argv[])
+{
+
+    DIR *dp;
+    struct dirent *dirp;
+    if(argc != 2)
+        err_quit("usage: ls directory_name");
+
+    return 0;
+}
